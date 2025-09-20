a3396d7777e15506328602e348ff6f4a532aa872
vanmai <admin@MaiDV.local>
2025-09-20 12:40:46 +0700

    setup database and graphql

---
+++ apps/api/src/modules/account/account.resolver.ts
@@ -0,0 +1,65 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Account, AccountStatus } from './account.entitty';
+import { AccountService } from './account.service';
+import { CreateAccountInput, UpdateAccountInput } from './dto/account.dto';
+import { AccountModel } from './models/account.model';
+
+@Resolver(() => AccountModel)
+export class AccountResolver {
+  constructor(private readonly accountService: AccountService) {}
+
+  // Queries
+  @Query(() => [AccountModel], { name: 'findAllAccounts' })
+  async findAll(): Promise<AccountModel[]> {
+    return this.accountService.findAll();
+  }
+
+  @Query(() => AccountModel, { name: 'findByIdAccount' })
+  async findById(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<AccountModel> {
+    return this.accountService.findById(id);
+  }
+
+  @Query(() => AccountModel, { name: 'findByEmailAccount' })
+  async findByEmail(
+    @Args('email', { type: () => String }) email: string,
+  ): Promise<AccountModel> {
+    return this.accountService.findByEmail(email);
+  }
+
+  @Query(() => [AccountModel], { name: 'findActiceAccount' })
+  async findActive(): Promise<AccountModel[]> {
+    return this.accountService.findActive();
+  }
+
+  // Mutations
+  @Mutation(() => AccountModel, { name: 'createAccount' })
+  async create(
+    @Args('input') input: CreateAccountInput,
+  ): Promise<AccountModel> {
+    return this.accountService.createAccount(input);
+  }
+
+  @Mutation(() => AccountModel, { name: 'updateAccount' })
+  async update(
+    @Args('id', { type: () => Int }) id: number,
+    @Args('input') input: UpdateAccountInput,
+  ): Promise<AccountModel> {
+    return this.accountService.updateAccount(id, input);
+  }
+
+  @Mutation(() => Boolean, { name: 'deleteAccount' })
+  async delete(@Args('id', { type: () => Int }) id: number): Promise<boolean> {
+    await this.accountService.deleteAccount(id);
+    return true;
+  }
+
+  @Mutation(() => AccountModel, { name: 'login' })
+  async login(
+    @Args('email') email: string,
+    @Args('password') password: string,
+  ): Promise<AccountModel> {
+    return this.accountService.validateLogin(email, password);
+  }
+}
---
+++ apps/api/src/modules/account/models/account.model.ts
@@ -0,0 +1,20 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { AccountStatus } from '../account.entitty';
+
+@ObjectType()
+export class AccountModel {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  email: string;
+
+  @Field(() => AccountStatus)
+  status: AccountStatus
+
+  @Field()
+  createdAt: Date;
+
+  @Field()
+  updatedAt: Date;
+}
